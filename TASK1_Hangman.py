4eecf914b39e778e857d3fd06a318a6a408023c8
Sai2475 <saisanjay3000@gmail.com>
2025-03-04 22:44:41 +0530

    Hangman game

--- TASK1_Hangman.py
+++ TASK1_Hangman.py
@@ -1,6 +1,6 @@
 import random 
 
-words = ("apple", "banana", "orange","coconut","pineapple")
+words = ("apple", "banana", "orange","coconut","pineapple","mango","straberry","lychee","watermelon","guava","lemon")
 
 hangman_art = {0: ("   ",
                    "   ",
@@ -25,19 +25,21 @@ hangman_art = {0: ("   ",
                    "/ \\")}
 
 def display_man(wrong_guesses):
+    print("***********")
     for line in hangman_art[wrong_guesses]:
         print(line)
+    print("************")
 
 def display_hint(hint):
-    pass
+    print(" ".join(hint))
 
 def display_answer(answer):
-    pass
+    print(" ".join(answer))
 
 def main():
     answer = random.choice(words)
     hint = ["_"] * len(answer)
-    wrong_guesses = 5
+    wrong_guesses = 0
     guessed_letters = set()
     is_running= True
 
@@ -45,6 +47,37 @@ def main():
         display_man(wrong_guesses)
         display_hint(hint)
         guess = input("Enter a letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("invalid input")
+            continue
+
+        if guess in guessed_letters:
+            print(f"{guess} is already guessed")
+            continue
+
+        guessed_letters.add(guess)
+
+        if guess in answer:
+           for i in range(len(answer)):
+            if answer[i] == guess:
+                hint[i] = guess
+
+        else:
+            wrong_guesses += 1
+
+        if "_" not in hint:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("YOU WIN !")
+            is_running = False
+
+        elif wrong_guesses >= len(hangman_art) - 1:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("YOU LOSE !")
+            is_running = False
+
 
 if __name__ == "__main__":
     main() 
