2169ece666d20e8d18f4570397655e86a063c5ed
Sai2475 <saisanjay3000@gmail.com>
2025-03-04 11:23:46 +0530

    started hangman.py

--- TASK1_Hangman.py
+++ TASK1_Hangman.py
@@ -1,1 +1,50 @@
-print("hello world")
+import random 
+
+words = ("apple", "banana", "orange","coconut","pineapple")
+
+hangman_art = {0: ("   ",
+                   "   ",
+                   "   "),
+               1: (" o ",
+                   "   ",
+                   "   "),
+               2: (" o ",
+                   " | ",
+                   "   "),
+               3: (" o ",
+                   "/| ",
+                   "   "),
+               4: (" o ",
+                   "/|\\",
+                   "   "),
+               5: (" o ",
+                   "/|\\",
+                   "/  "),
+               6: (" o ",
+                   "/|\\",
+                   "/ \\")}
+
+def display_man(wrong_guesses):
+    for line in hangman_art[wrong_guesses]:
+        print(line)
+
+def display_hint(hint):
+    pass
+
+def display_answer(answer):
+    pass
+
+def main():
+    answer = random.choice(words)
+    hint = ["_"] * len(answer)
+    wrong_guesses = 5
+    guessed_letters = set()
+    is_running= True
+
+    while is_running:
+        display_man(wrong_guesses)
+        display_hint(hint)
+        guess = input("Enter a letter: ").lower()
+
+if __name__ == "__main__":
+    main() 
