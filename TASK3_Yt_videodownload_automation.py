b09b7f81995c612726d6d87285dbcb8863c51abb
Sai2475 <saisanjay3000@gmail.com>
2025-03-11 15:27:11 +0530

    Yt videodownload

--- TASK3_Yt_videodownload_automation.py
+++ TASK3_Yt_videodownload_automation.py
@@ -8,6 +8,19 @@ try:
     print("Title:", yt.title)
     print("Views:", yt.views)
 
+    yd = yt.streams.get_highest_resolution()
+    #downloading the video
+    yd.download()
+
+    print("download complete")
+except Exception as e :
+ print("an error occured :", str(e))
+
+
+
+
+
+
 
 
 
